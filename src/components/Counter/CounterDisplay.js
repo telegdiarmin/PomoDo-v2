cd0cd2fd7cc160b123cbbc2d6c75b93c27867134
Telegdi Ármin <armin.telegdi@gmail.com>
2022-07-05 23:02:57 +0200

    Added counter to page title.

--- src/components/Counter/CounterDisplay.js
+++ src/components/Counter/CounterDisplay.js
@@ -6,13 +6,9 @@ import { formatTime } from "../../utils/utils";
 import classes from "./CounterDisplay.module.css";
 
 const CounterDisplay = () => {
-  const {
-    mode,
-    remainingTime,
-    timerDuration,
-    shortBreakDuration,
-    longBreakDuration,
-  } = useSelector((state) => state.counter);
+  const { mode, remainingTime, timerDuration } = useSelector(
+    (state) => state.counter
+  );
 
   const [displayTime, setDisplayTime] = useState(formatTime(timerDuration));
 
@@ -20,6 +16,22 @@ const CounterDisplay = () => {
     const time = formatTime(remainingTime);
     setDisplayTime(time);
   }, [remainingTime]);
+
+  useEffect(() => {
+    let message;
+    switch (mode) {
+      case "timer":
+        message = " - Koncentrálj!";
+        break;
+      case "shortBreak":
+        message = " - Tarts szünetet!";
+        break;
+      case "longBreak":
+        message = " - Lazíts egy kicsit!";
+        break;
+    }
+    document.title = `${displayTime} ${message}`;
+  }, [displayTime, mode]);
 
   return <div className={classes.counterDisplay}>{displayTime}</div>;
 };
